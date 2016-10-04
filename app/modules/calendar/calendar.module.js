8128b900994e23e88368bb101d68d0119f366c76
Ander Conal <ander.conal@jinnapp.com>
2016-10-04 10:16:30 +0200

    Configure how the calendar loads by default

--- app/modules/calendar/calendar.module.js
+++ app/modules/calendar/calendar.module.js
@@ -1,5 +1,18 @@
 (function() {
   'use strict';
 
-  angular.module('calendar', []);
+  angular.module('calendar', [])
+    /* @ngInject */
+    .config(function(calendarConfig) {
+      // Use moment to format dates
+      calendarConfig.dateFormatter = 'moment';
+
+      /* This will configure times on the day view to display in 24 hour format
+         rather than the default of 12 hour */
+      calendarConfig.allDateFormats.moment.date.hour = 'HH:mm';
+      
+      /* Make the week view more like the day view, with the caveat that event
+         end times are ignored. */
+      calendarConfig.showTimesOnWeekView = true;
+    });
 })();
